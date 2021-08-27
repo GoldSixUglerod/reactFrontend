adb60408946a43f2e7a1ad77d2e0706cac418307
Vladimir <v.makharev@yandex.ru>
2021-08-27 23:53:33 +0300

    Add Linter + Prettier, tree example

--- uglerod-frontend/src/App.tsx
+++ uglerod-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'antd';
+import { OrgChartTree } from './Tree';
 
 const App = () => {
   return (
@@ -9,11 +10,12 @@ const App = () => {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
       }}
     >
       <Button type="primary">Hello, Ant Design!</Button>
       <a href="foo.bar">I'm a link. Click me, please!</a>
+      <OrgChartTree />
     </main>
   );
 };
