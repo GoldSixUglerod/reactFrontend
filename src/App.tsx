9ca151f2a5188d60303013358df46a89ea7f25db
Artem Batalov <a@batalov.me>
2021-08-28 17:05:26 +0300

    MaterialUI start

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,93 @@
 import React from 'react';
+import {
+    AppBar,
+    Button,
+    CssBaseline,
+    Divider,
+    Drawer,
+    Hidden,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography,
+    useTheme,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/Inbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 import { MainTree } from './components/Tree';
 
 const App: React.FC = () => {
+    const theme = useTheme();
+    const container = window !== undefined ? () => window.document.body : undefined;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const drawer = (
+        <div>
+            <div />
+            <Divider />
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
     return (
-        <main
-            style={{
-                display: 'flex',
-                flex: 'min-content',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
+        <div>
+            <CssBaseline />
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="open drawer" edge="start">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Responsive drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <nav aria-label="mailbox folders">
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                        open={mobileOpen}
+                        onClose={() => {
+                            setMobileOpen(!mobileOpen);
+                        }}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer variant="permanent" open>
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+            </nav>
             <MainTree />
-        </main>
+        </div>
     );
 };
 
